e443b1facff6dd9b9b64bd7f2a78e6e7cb2b2d64
Reginaldo Filho <regifilho06@gmail.com>
2023-11-27 09:59:48 -0300

    Remove error package

--- webapp/src/assets/aux/controller.js
+++ webapp/src/assets/aux/controller.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+const DB_ADDRESS = process.env.DB_ADDRESS
+const DB_PORT = process.env.DB_PORT
+
 export const controller = axios.create({
-    baseURL: '18.228.153.168:8080'
+    baseURL: `${DB_ADDRESS}:${DB_PORT}`
 })
